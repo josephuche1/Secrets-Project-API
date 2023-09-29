9fa1794a90df312c32458ae36433d7a00b99f176
PARSIVALJ <uchejesusboy@gmail.com>
2023-09-29 17:33:08 +0300

    imported dependencies, created express app and set port number, rendered index.ejs to home page

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 // HINTS:
 // 1. Import express and axios
+import express from "express";
+import x from "axios";
 
 // 2. Create an express app and set the port number.
+const app = express();
+const port = 3000;
 
 // 3. Use the public folder for static files.
+app.use(express.static("public"));
 
 // 4. When the user goes to the home page it should render the index.ejs file.
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+})
 
 // 5. Use axios to get a random secret and pass it to index.ejs to display the
 // secret and the username of the secret.
